3abfda258234ce05db25d2c2c84a11b01a8b94d2
Pablo Díaz <pablo5_diaz@hotmail.com>
2017-06-01 16:24:34 -0600

    start game button

---
+++ Assets/ButtonOnClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonOnClick : MonoBehaviour {
+
+	public GameObject menu;
+	public Button startButton;
+
+	// Use this for initialization
+	void Start () {
+		startButton = GameObject.Find("Continue").GetComponent<Button>();
+		startButton.onClick.AddListener(TaskOnClick);
+		menu = GameObject.Find ("MainMenu");
+
+
+	}
+
+	void TaskOnClick()
+	{
+		menu.SetActive (false);
+	}
+
+}
--- Assets/pacman.cs
+++ Assets/pacman.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class pacman : MonoBehaviour {
 
@@ -12,14 +13,24 @@ public class pacman : MonoBehaviour {
 	public float verticalSpeed = 2.0F;
 	public GameObject cameraPlayer;
 	public GameObject cameraMenu;
+	public Button newGame;
+	public GameObject menu;
+	public Button startButton;
+
 	// Use this for initialization
 	void Start () {
 		pacm = GameObject.Find ("pacman");
 		cameraPlayer = GameObject.Find ("Camera");
 		cameraMenu = GameObject.Find ("CameraMenu");
+		menu = GameObject.Find ("MainMenu");
+
 		cameraPlayer.SetActive (false);
-		//cameraMenu.SetActive(false);
+		startButton = GameObject.Find("Continue").GetComponent<Button>();
+		startButton.onClick.AddListener(startOnClick);
+		menu = GameObject.Find ("MainMenu");
+
 		Debug.Log ("test");
+
 	}
 	
 	// Update is called once per frame
@@ -40,7 +51,7 @@ public class pacman : MonoBehaviour {
 		float h = horizontalSpeed * Input.GetAxis("PS4_RIGHTANALOG_HORIZONTAL");
 		float v = rotationSpeed * Input.GetAxis ("PS4_RIGHTANALOG_VERTICAL");
 		Debug.Log (v);
-		pacm.transform.Rotate(v, 0, 0);
+		pacm.transform.Rotate(v, h, 0);
 		float translation = Input.GetAxis("Vertical") * moveSpeed;
 		float translationX = Input.GetAxis("Horizontal") * moveSpeed;
 		translation *= Time.deltaTime;
@@ -49,5 +60,12 @@ public class pacman : MonoBehaviour {
 		pacm.transform.Translate(0, 0, translationX);
 
 
+	}
+
+	void startOnClick()
+	{
+		menu.SetActive (false);
+		cameraMenu.SetActive(false);
+		cameraPlayer.SetActive (true);
 	}
 }
